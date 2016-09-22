b73b11706d5d1540e63b75ed050a07bb31b0d042
singleparty <singleparty2012@gmail.com>
2016-09-22 16:46:34 +0800

    store hot update

--- src/designer/js/store.js
+++ src/designer/js/store.js
@@ -2,7 +2,7 @@ import Vuex from 'libs/vuex-1.0.0/vuex.js';
 import Vue from 'libs/vue-1.0.26/vue.dev.js';
 Vue.use(Vuex);
 const state = {
-    count: 0,
+    count: 1,
     a: {}
 };
 const mutations = {
@@ -11,5 +11,16 @@ const mutations = {
     }
 };
 export default new Vuex.Store({
+    strict: __DEV__,
     state, mutations
 });
+if(__DEV__) {
+    if (module.hot) {
+        module.hot.accept(['./actions'], () => {
+            const newActions = require('./actions').default;
+            store.hotUpdate({
+                actions: newActions
+            })
+        })
+    }
+}
