2a399d879e03e080c613340eac3a4bdb60014136
singleparty <singleparty2012@gmail.com>
2016-11-09 11:52:48 +0800

    editUrl

---
+++ src/designer/edts/editUrl/editUrl.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="edit-url">
+        
+    </div>
+</template>
+<style lang="less" scoped>
+    @import "./style.less";
+</style>
+<script type="es6">
+    var ctor = Vue.extend({
+        props: ['option'],
+        data() {
+            return {}
+        },
+        methods: {
+
+        }
+    });
+    export default ctor;
+</script> 
