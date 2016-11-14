0ce0d7e291f265a4917da155a2b8ade22ad10989
singleparty <singleparty2012@gmail.com>
2016-11-14 15:01:54 +0800

    精简main.js

--- src/designer/js/main.js
+++ src/designer/js/main.js
@@ -6,14 +6,12 @@ import operation from 'designer/components/operation/operation.vue';
 import uploadImg from 'designer/plugin/uploadImg/uploadImg';
 import toast from 'designer/components/toast/toast.vue';
 import dialog from 'designer/components/dialog/dialog.vue';
-import {mapActions} from 'vuex';
 import store from 'store/index';
 Vue.use(uploadImg, {
     url: '../backend/uploadImg.php', //上传地址
     fileDataName: 'upload', //上传的name
     maxFileSize: 5 * 1024 * 1024 //图片上限
 });
-const _actions = mapActions(['init']);
 const main = new Vue({
     el: document.getElementById('main'),
     data: {},
@@ -21,10 +19,7 @@ const main = new Vue({
         menus, editors, screen, operation, toast, dialog
     },
     ready: function () {
-        this.init();
-    },
-    methods: {
-        ..._actions
+        this.$store.dispatch('init');
     },
     store
 });
